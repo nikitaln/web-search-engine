41cb0146761ad41f320198caa8c0f1e45911cd67
Nameзадаем <nikitaln7@gmail.com>
2023-04-07 15:09:56 +0300

    morphology service added

--- src/main/java/searchengine/services/morphology/MorphologyService.java
+++ src/main/java/searchengine/services/morphology/MorphologyService.java
@@ -1,21 +1,39 @@
 package searchengine.services.morphology;
 
+import org.apache.lucene.morphology.LuceneMorphology;
+import org.apache.lucene.morphology.russian.RussianLuceneMorphology;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MorphologyService {
 
     public void getLemma(String text) {
-        
+
+        List<String> allString = new ArrayList<>();
+
         String regex = "[.,-]+";
 
         text = text.replaceAll(regex, "");
 
-
         String[] words = text.split("\s");
 
         for(int i = 0; i < words.length; i++) {
-            System.out.println(words[i]);
-            if (words[i].isEmpty()) {
+
+            LuceneMorphology luceneMorphology = null;
+            try {
+                luceneMorphology = new RussianLuceneMorphology();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
             }
+
+            List<String> wordBaseForms = luceneMorphology.getNormalForms(words[i]);
+            allString.addAll(wordBaseForms);
         }
 
+        for (String word : allString) {
+            System.out.println(word);
+        }
     }
 }
